84337837795b660c70efd29e42870f88f64a1512
RK <rockingengineer02@gmail.com>
2018-08-10 01:47:51 +0800

    Removed code for windows tag

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -61,17 +61,7 @@ class BlogPostTemplate extends React.Component {
             </li>
           )}
         </ul>
-        <script>
-          if (window.netlifyIdentity) {
-            window.netlifyIdentity.on("init", user => {
-              if (!user) {
-                window.netlifyIdentity.on("login", () => {
-                  document.location.href = "/admin/";
-                });
-              }
-            })
-          }
-        </script>
+       
       </Layout>
     )
   }
